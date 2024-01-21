ff8d1bc85dcec5d0372f0e7434273d9f4746e15f
ValldemarXorin <154762267+ValldemarXorin@users.noreply.github.com>
2024-01-22 01:23:55 +0300

    Update 16.cpp

--- First_Theme/Дайте9.cpp
+++ First_Theme/Дайте9.cpp
@@ -1,12 +1,12 @@
-//#include <stdio.h>
-//#include <stdlib.h>
-//
-//
-//int main()
-//{
-//	int n;
-//	scanf_s("%d", &n);
-//	printf("No");
-//	return 0;
-//}
-// 16 zadacha
+#include <stdio.h>
+#include <stdlib.h>
+
+
+int main()
+{
+	int n;
+	scanf_s("%d", &n);
+	printf("No");
+	return 0;
+}
+ //16 zadacha. Чисто математически такого не может быть.
